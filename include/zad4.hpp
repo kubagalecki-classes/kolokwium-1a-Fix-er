4ca87ac764ad8326e52130b5e67c0a94f2d0a11f
Fixer1 <figat.wcale.nie@gmail.com>
2021-01-14 12:59:01 +0000

    44

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -6,19 +6,18 @@
 double obliczMake(std::vector<Tagliatelle> v){
   using namespace std;
   double M=0;
-for_each(v.end(),v.begin(),[&](Tagliatelle it){
   int i=1;
+  for_each(v.rbegin(),v.rend(),[&](Tagliatelle it){
+  
  double N = it.ileMaki(i);
  M += N;
   i++;
-  return M;
 });
-
 if (M>100){
 int c=50;
 throw (c);}
 else if(M>50){
-  double d=50;
+double d=50;
 throw (d);
 }else{
   return M;
