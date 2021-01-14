8f073ed2d97ffef2429b779d6f3a909e377d0b0d
Fixer1 <figat.wcale.nie@gmail.com>
2021-01-14 12:03:56 +0000

    41

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -4,7 +4,8 @@
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
 class Makaron{
-virtual double ileMaki(unsigned P) const=0;
+public:
+virtual double ileMaki(unsigned) const=0;
 static Makaron* gotujMakaron(std::string s);
 };
 // Zad1
