43562cef9c0f3ab9099363bb785d9615d62cde7c
Fixer1 <figat.wcale.nie@gmail.com>
2021-01-14 11:46:19 +0000

    2

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -4,11 +4,12 @@
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
 class Makaron{
-
+double ileMaki(unsigned) const=0;
+static Makaron* gotujMakaron(std::string s);
 };
 // Zad1
 // tutaj definicja klasy Tagliatelle
-class Tagliatelle{
+class Tagliatelle : public Makaron{
 public:
 Tagliatelle(double l, double w, double r):L(l),W(w),R(r){}
 Tagliatelle(): L(0.5),W(0.5),R(0.5){}
--- include/zad2.hpp
+++ include/zad2.hpp
@@ -5,5 +5,16 @@
 #include <type_traits>
 
 // tutaj klasa Penne
-
+class Penne : public Makaron{
+double ileMaki(unsigned P)double{
+  return P;
+};
+};
 // tutaj definicja metody gotujMakaron
+Makaron* Makaron::gotujMakaron(std::string s){
+  if(s.front()==s.back){
+    return new Tagliatelle(3.14,0.42,0.1);
+  }else{
+    return new Penne;
+  }
+};
